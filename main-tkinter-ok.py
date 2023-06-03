4e23574af3952f09be9e19e5f8e18f6b4e12e206
Mandarin <zurual@mail.ru>
2023-06-03 19:12:07 +0300

    изменение фона кaртинки

--- main-tkinter-ok.py
+++ main-tkinter-ok.py
@@ -24,7 +24,7 @@ tk.wm_attributes("-topmost", 1)
 sc = Canvas(tk, width=RES[0], height=RES[1], bg="red", highlightthickness=0)
 sc.pack()
 
-game_sc = Canvas(tk, width=W*TILE+1, height=H*TILE+1, bg="yellow", highlightthickness=0)
+game_sc = Canvas(tk, width=W*TILE+1, height=H*TILE+1, bg="blue", highlightthickness=0)
 game_sc.place(x=20, y=20, anchor=NW)
 
 img_obj1 = PhotoImage(file="img/bg.png")
@@ -35,6 +35,19 @@ game_sc.create_image(0, 0, anchor=NW, image=img_obj2)
 
 grid = [game_sc.create_rectangle(x * TILE, y * TILE, x * TILE+TILE, y * TILE+TILE) for x in range(W) for y in range(H)]
 
+figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
+               [(0, -1), (-1, -1), (-1, 0), (0, 0)],
+               [(-1, 0), (-1, 1), (0, 0), (0, -1)],
+               [(0, 0), (-1, 0), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, 0)]]
+
+figures = [[[x + W // 2, y + 1, 1, 1] for x, y in fig_pos] for fig_pos in figures_pos]
+#figure_rect = pygame.Rect(0, 0, TILE - 2, TILE - 2)
+field = [[0 for i in range(W)] for j in range(H)]
+
+anim_count, anim_speed, anim_limit = 0, 60, 2000
 
 score = "0"
 record = "0"
@@ -51,18 +64,6 @@ get_color = lambda : (randrange(30, 256), randrange(30, 256), randrange(30, 256)
 def rgb_to_hex(rgb):
     return '#%02x%02x%02x' % rgb
 
-
-figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
-               [(0, -1), (-1, -1), (-1, 0), (0, 0)],
-               [(-1, 0), (-1, 1), (0, 0), (0, -1)],
-               [(0, 0), (-1, 0), (0, 1), (-1, -1)],
-               [(0, 0), (0, -1), (0, 1), (-1, -1)],
-               [(0, 0), (0, -1), (0, 1), (1, -1)],
-               [(0, 0), (0, -1), (0, 1), (-1, 0)]]
-
-figures = [[(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
-#figure_rect = pygame.Rect(0, 0, TILE - 2, TILE - 2)
-field = [[0 for i in range(W)] for j in range(H)]
 
 from copy import deepcopy
 figure, next_figure = deepcopy(choice(figures)), deepcopy(choice(figures))
